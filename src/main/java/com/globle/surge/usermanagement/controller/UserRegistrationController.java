a0a22add6bfae8d52746290df513e7f05cb56efa
Mohamed Safran <safranfaiz@yahoo.com>
2021-10-03 16:52:19 +0530

    code refactor

--- src/main/java/com/globle/surge/usermanagement/controller/UserRegistrationController.java
+++ src/main/java/com/globle/surge/usermanagement/controller/UserRegistrationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserRegistration {
+public class UserRegistrationController {
 
     @Autowired
     UserService userService;
--- src/main/java/com/globle/surge/usermanagement/repository/UserRepository.java
+++ src/main/java/com/globle/surge/usermanagement/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.globle.surge.usermanagement.repository;
 
 import com.globle.surge.usermanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByEmail(String email);
 }
